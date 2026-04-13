56a728442eebeaa87e7a1d5062de9387f6331623
shaynewanjiru <shayne.wanjiru@student.moringaschool.com>
2026-04-13 22:32:28 +0300

    lab for modern js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const args = { }
+function combineUsers(...args) {
+  const combineObject ={ users: []}
+  let args = i
+  for (let i = 0; i < args.length; i++) {
+    combineObject.users.push(...args[i])
+  } 
+  combineObject.merge_date = Date.today().toString("M/d/yyyy")
+  return combineObject
+}           
+
 
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+};  
+
+
+
